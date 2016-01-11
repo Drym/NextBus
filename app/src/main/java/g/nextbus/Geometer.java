85634ff48d092c78409a17eb33f6fc17971931e4
Jonathan Boudab <jonathan.boudab@etu.unice.fr>
2016-01-11 16:02:38 +0100

    NEXG-23 mise en place d'un debut d'algo pour donner le meilleur arret et mise en place de la convertion adresse / position GPS

---
+++ app/src/main/java/g/nextbus/Geometer.java
@@ -0,0 +1,60 @@
+package g.nextbus;
+
+/**
+ * Created by user on 11/01/16.
+ */
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.List;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Geometer {
+
+    private static final String URL = "http://maps.googleapis.com/maps/api/geocode/json";
+
+    public String getJSONByGoogle(String fullAddress) throws IOException {
+
+        URL url = new URL(URL + "?address=" + URLEncoder.encode(fullAddress, "UTF-8")+ "&sensor=false");
+
+        URLConnection conn = url.openConnection();
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream(1024);
+
+        IOUtils.copy(conn.getInputStream(), output);
+
+        String out = output.toString();
+
+        try {
+            JSONObject jsonObject = new JSONObject(out);
+
+            double lng = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lng");
+
+            double lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lat");
+
+            System.out.println(lat);
+
+            System.out.println(lng);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return out;
+
+    }
+
+}
