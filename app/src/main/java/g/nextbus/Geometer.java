bd1db83b19b309f4e61638393f716bbfe504915c
Jonathan Boudab <jonathan.boudab@etu.unice.fr>
2016-01-12 10:32:08 +0100

    NEXG-23 test fix bug geoposition adresse

--- app/src/main/java/g/nextbus/Geometer.java
+++ app/src/main/java/g/nextbus/Geometer.java
@@ -6,9 +6,17 @@ package g.nextbus;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -19,11 +27,23 @@ public class Geometer {
     double lng;
     double lat;
     LatLng latLng;
+    private InputStream is;
 
-    public InputStream getJSONByGoogle(String fullAddress) throws IOException {
+
+    public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line + "\n");
+        }
+        return builder.toString();
+    }
+
+    public String getJSONByGoogle(String fullAddress) throws IOException {
 
 
-        URL url = new URL(URL + "?address=" + URLEncoder.encode(fullAddress, "UTF-8")+ "&sensor=false");
+        /*URL url = new URL(URL + "?address=" + URLEncoder.encode(fullAddress, "UTF-8")+ "&sensor=false");
 
         URLConnection conn = url.openConnection();
 
@@ -31,7 +51,7 @@ public class Geometer {
         ByteArrayOutputStream output = new ByteArrayOutputStream(1024);
 
         return conn.getInputStream();
-
+        */
         /*
         IOUtils.copy(conn.getInputStream(), output);
 
@@ -62,6 +82,45 @@ public class Geometer {
         }
         */
        // return "aaaa";
+
+
+
+        URL url = new URL(URL + "?address=" + URLEncoder.encode(fullAddress, "UTF-8")+ "&sensor=false");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Starts the query
+        conn.connect();
+        int response = conn.getResponseCode();
+        is = conn.getInputStream();
+
+        // Convert the InputStream into a string
+        String contentAsString = readIt(is);
+
+
+        try {
+            JSONObject jsonObject = new JSONObject(contentAsString);
+
+            double lng = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lng");
+
+            double lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lat");
+
+            System.out.println(lat);
+            System.out.println(lng);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return contentAsString;
+
 
     }
 
