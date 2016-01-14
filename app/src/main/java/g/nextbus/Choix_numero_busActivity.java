9906ac0a07367130f27be529f1704cfe57f65239
Jonathan Boudab <jonathan.boudab@etu.unice.fr>
2016-01-14 10:49:16 +0100

    NEXG-2 modification du menu pour les lignes de bus

--- app/src/main/java/g/nextbus/Choix_numero_busActivity.java
+++ app/src/main/java/g/nextbus/Choix_numero_busActivity.java
@@ -11,3 +11,11 @@ import fr.rolandl.blog_gps.R;
  * Created by user on 14/01/16.
 **/
 
+public class Choix_numero_busActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choix_numero_bus);
+    }
+}
